6e1278f65b681cca0d66a810e6da6abdf98af069
Yuki <yuki08281993@gmail.com>
2017-11-03 17:32:29 +0900

    [FIX] Make the "quit" command work correctly.

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,8 +15,8 @@ repl :: IO ()
 repl = do
     putStr ">" ; hFlush stdout
     str <- getLine
-    when (str `elem` ["q", "Q", "quit", "Quit", "QUIT"]) (return ())
-    case Exp.parse str of
-         Left e -> putStrLn e
-         Right e -> printLn e
-    repl
+    unless (str `elem` ["q", "Q", "quit", "Quit", "QUIT"]) $ do
+        case Exp.parse str of
+            Left e -> putStrLn e
+            Right e -> printLn e
+        repl
