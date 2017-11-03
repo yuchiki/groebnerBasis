9ebc7698fc46e52b708083ddc376518dc0b81cc7
Yuki <yuki08281993@gmail.com>
2017-11-04 00:50:43 +0900

    [ADD] Add test for multiplication between exp and exp.

--- test/ExpSpec.hs
+++ test/ExpSpec.hs
@@ -9,9 +9,11 @@ spec = do
         context "when expression has const" $
             it "should parse" $
                 parshow "(2+1)^2" `shouldBe` "9"
-        context "when nested expression" $
+        context "when nested expression" $ do
             it "should flatten expression" $
                 parshow "a(b+c)" `shouldBe` "ab+ac"
+            it "should deal coefficient correctly" $
+                parshow "(a+b)^2" `shouldBe` "a^2+2ab+b^2"
     describe "show" $
         context "when 'na' is given" $ do
             it "should not show 1 in 1a" $
